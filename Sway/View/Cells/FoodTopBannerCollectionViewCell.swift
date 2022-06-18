136b09e1df07bc83222fe6f0be56f86d2e35ad33
omarradwan <oradwan037@gmail.com>
2022-06-18 16:41:17 +0200

    Update FoodTopBannerCollectionViewCell.swift

--- Sway/View/Cells/FoodTopBannerCollectionViewCell.swift
+++ Sway/View/Cells/FoodTopBannerCollectionViewCell.swift
@@ -29,6 +29,7 @@ class FoodTopBannerCollectionViewCell: UICollectionViewCell {
         bI.clipsToBounds = true
         bI.contentMode = .scaleAspectFill
         bI.layer.cornerRadius = 8
+        
         return bI
     }()
     
