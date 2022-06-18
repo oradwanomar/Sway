0dfc7bae3a7d895fc44f60b0af717c4d687cf7cf
omarradwan <oradwan037@gmail.com>
2022-06-18 16:42:10 +0200

    Update FoodCategoryCollectionViewCell.swift

--- Sway/View/Cells/FoodCategoryCollectionViewCell.swift
+++ Sway/View/Cells/FoodCategoryCollectionViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class FoodCategoryCollectionViewCell: UICollectionViewCell {
     
-    
+    //MARK: Properities
     
     static let cellIdentifier = "FoodCategoryCollectionViewCell"
     
