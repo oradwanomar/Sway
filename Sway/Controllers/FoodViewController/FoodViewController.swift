18c7b1ded6b23f846f00b865be8083eab3e586fd
omarradwan <oradwan037@gmail.com>
2022-06-25 19:54:00 +0200

    Update FoodViewController.swift

--- Sway/Controllers/FoodViewController/FoodViewController.swift
+++ Sway/Controllers/FoodViewController/FoodViewController.swift
@@ -74,7 +74,6 @@ class FoodViewController: UIViewController {
         view.addSubview(collectionView)
         view.addSubview(filterHeaderView)
     }
-
     
     func setUpConstrains(){
         collectionView.setUp(to: view)
