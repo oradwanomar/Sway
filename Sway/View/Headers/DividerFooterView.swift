6a1563dc4d9268ec1a2eaf45569dda49d9377bbb
omarradwan <oradwan037@gmail.com>
2022-06-25 19:55:30 +0200

    Update DividerFooterView.swift

--- Sway/View/Headers/DividerFooterView.swift
+++ Sway/View/Headers/DividerFooterView.swift
@@ -38,6 +38,7 @@ class DividerFooterView: UICollectionReusableView {
         ])
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
