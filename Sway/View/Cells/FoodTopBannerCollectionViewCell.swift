b97ff2ec49c14c0fc556b178c48393c033c6829d
omarradwan <oradwan037@gmail.com>
2022-07-07 19:47:01 +0200

    Update FoodTopBannerCollectionViewCell.swift

--- Sway/View/Cells/FoodTopBannerCollectionViewCell.swift
+++ Sway/View/Cells/FoodTopBannerCollectionViewCell.swift
@@ -15,8 +15,7 @@ class FoodTopBannerCollectionViewCell: UICollectionViewCell {
     
     var cellData : FoodTopBannerModel? {
         didSet {
-            guard let cellData = cellData else {
-return}
+            guard let cellData = cellData else {return}
             bannerImage.image = UIImage(named: cellData.image)
         }
     }
